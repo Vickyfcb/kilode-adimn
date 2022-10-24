df5403a8e2b98a1640625ed8a551c41dd565537d
ennyworld <ennyjay4you@gmail.com>
2022-10-24 20:41:11 +0100

    Updated

--- js/custom-script.js
+++ js/custom-script.js
@@ -3,41 +3,53 @@
 // Toggle the side navigation
 const sidebarToggle = document.body.querySelector('#sidebarToggle');
 if (sidebarToggle) {
-  // Uncomment Below to persist sidebar toggle between refreshes
-  if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-    document.body.classList.toggle('sb-sidenav-toggled');
-  }
-  sidebarToggle.addEventListener('click', event => {
-    event.preventDefault();
-    document.body.classList.toggle('sb-sidenav-toggled');
-    localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-  });
+	// Uncomment Below to persist sidebar toggle between refreshes
+	if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+		document.body.classList.toggle('sb-sidenav-toggled');
+	}
+	sidebarToggle.addEventListener('click', event => {
+		event.preventDefault();
+		document.body.classList.toggle('sb-sidenav-toggled');
+		localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+	});
 }
 
 // Data Table Script
-$(document).ready(function () {
-  var table = $('#example').DataTable({
-    rowReorder: {
-      selector: 'td:nth-child(2)'
-    },
-    responsive: true
-  });
-});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
+
+
+
+
 
 // Delete Row Button
 $('a[type="button"]').click(function (e) {
-  $(this).closest('tr').remove()
+	$(this).closest('tr').remove()
 })
 
 // Select All Button
 $("#selectAll").click(function () {
-  $("input[type=checkbox]").prop("checked", $(this).prop("checked"));
+	$("input[type=checkbox]").prop("checked", $(this).prop("checked"));
 });
 
 $("input[type=checkbox]").click(function () {
-  if (!$(this).prop("checked")) {
-    $("#selectAll").prop("checked", false);
-  }
+	if (!$(this).prop("checked")) {
+		$("#selectAll").prop("checked", false);
+	}
 });
 
 jackHarnerSig();
@@ -45,11 +57,11 @@ jackHarnerSig();
 
 // For Select Option
 $("select").click(function () {
-  var open = $(this).data("isopen");
-  if (open) {
-    window.location.href = $(this).val()
-  }
-  //set isopen to opposite so next time when use clicked select box
-  //it wont trigger this event
-  $(this).data("isopen", !open);
+	var open = $(this).data("isopen");
+	if (open) {
+		window.location.href = $(this).val()
+	}
+	//set isopen to opposite so next time when use clicked select box
+	//it wont trigger this event
+	$(this).data("isopen", !open);
 });
